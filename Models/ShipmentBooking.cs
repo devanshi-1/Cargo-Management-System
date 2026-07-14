860c649fc325047da3a75ac11d09c0f786cc778b
Shukla <2507429@cognizant.com>
2026-07-14 17:35:45 +0530

    Modified Shipment Model

--- Models/ShipmentBooking.cs
+++ Models/ShipmentBooking.cs
@@ -5,19 +5,37 @@ namespace CargoManagementSystem.Models
     public enum BookingStatus { DRAFT, CONFIRMED, CANCELLED, COMPLETED }
 
     public class ShipmentBooking
-    {        
+    {
         [Key]
         public int BookingId { get; set; }
-        public string BookingNumber { get; set; }
-        public string ShipperName { get; set; }
-        public string ConsigneeName { get; set; }
-        public string OriginPort { get; set; }
-        public string DestinationPort { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string BookingNumber { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string ShipperName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string ConsigneeName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string OriginPort { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string DestinationPort { get; set; } = string.Empty;
+
+        [Required]
         public BookingStatus BookingStatus { get; set; }
 
         // Navigation Properties for related records
         public ICollection<Container> Containers { get; set; }
         public ICollection<CustomsDeclaration> CustomsDeclarations { get; set; }
         public ICollection<FreightInvoice> FreightInvoices { get; set; }
+
     }
 }
