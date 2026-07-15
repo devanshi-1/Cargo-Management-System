0379c777dd6951b5ceddc11c9adafe5857121890
Rathaur <2507515@cognizant.com>
2026-07-15 10:32:07 +0530

    Implemented IShipmentBookingService layer and controller

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
-using Microsoft.EntityFrameworkCore;
 using Cargo_Management_Project.Data;
+using Cargo_Management_Project.Services;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<IShipmentBookingService, ShipmentBookingService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
